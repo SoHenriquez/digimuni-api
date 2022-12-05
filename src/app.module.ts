69ca2e6bf7a196d2c2f66b599adbbc312a051805
SoHenriquez <estebansotohenriquez@gmail.com>
2022-12-05 02:47:00 -0300

    estructura base con .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+
+import { SocialApiModule } from './modules/social-api/social-api.module';
+import { CommonModule } from './common/common.module';
+import { EnvConfiguration } from './config/env.config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SocialApiModule } from './modules/social-api/social-api.module';
 
 @Module({
-  imports: [SocialApiModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+    }),
+    SocialApiModule,
+    CommonModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/modules/social-api/dto/update-social-api.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSocialApiDto } from './create-social-api.dto';
-
-export class UpdateSocialApiDto extends PartialType(CreateSocialApiDto) {}
