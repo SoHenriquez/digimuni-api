d3a8a062ad2b32e06a34b31d6e014ef141fe7b4d
SoHenriquez <estebansotohenriquez@gmail.com>
2022-12-06 02:01:36 -0300

    use guards on url login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 
-@Module({})
-export class AuthModule {}
+import { AuthService } from './services/auth.service';
+import { UsersModule } from 'src/modules/social-api/users/users.module';
+import { LocalStrategy } from './strategies/local.strategy';
+import { AuthController } from './controllers/auth.controller';
+
+@Module({
+  providers: [AuthService, LocalStrategy],
+  imports: [UsersModule, PassportModule],
+  controllers: [AuthController],
+})
+export class AuthModule { }
