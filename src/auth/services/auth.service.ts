c38c9ca06d2b0a2fa13f2d217c8eba4b60f5a0dc
SoHenriquez <estebansotohenriquez@gmail.com>
2022-12-09 01:16:21 -0300

    function generate jwt and validation modification

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from 'src/modules/social-api/users/users.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+import { UsersService } from 'src/modules/social-api/users/users.service';
+import { User } from './../../modules/social-api/users/entities/user.entity';
+import { PayloadToken } from './../models/token.model';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) { }
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService
+    ) { }
 
     async validateUser(email: string, password: string) {
 
@@ -13,11 +20,19 @@ export class AuthService {
         if (user) {
             const isMatch = await bcrypt.compare(password, user.password);
             if (isMatch) {
-                const { password, ...rta } = user.toJSON();
-                return rta;
+                const token = await this.generateJWT(user);
+                return {
+                    user,
+                    token,
+                };
             }
 
         }
         return null;
+    }
+
+    generateJWT(user: User) {
+        const payload: PayloadToken = { role: user.role, sub: user._id };
+        return  this.jwtService.sign(payload);
     }
 }
