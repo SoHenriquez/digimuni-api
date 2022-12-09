5f78c7ecfd8c59c45eafde757ce5dba78334c9a6
SoHenriquez <estebansotohenriquez@gmail.com>
2022-12-09 01:24:24 -0300

    type payload

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,33 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthService } from './services/auth.service';
 import { UsersModule } from 'src/modules/social-api/users/users.module';
 import { LocalStrategy } from './strategies/local.strategy';
 import { AuthController } from './controllers/auth.controller';
+import config from './../config/config';
 
 @Module({
+  imports: [
+    UsersModule, 
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService ) => {
+        console.log(configService.get('JWT_SECRET'));
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '10s',
+          },
+        };
+      },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
-  imports: [UsersModule, PassportModule],
   controllers: [AuthController],
 })
 export class AuthModule { }
