f589e2c6259f420cc9f3faf9b0697a19950b032c
SoHenriquez <estebansotohenriquez@gmail.com>
2022-12-09 01:30:02 -0300

    use guards with local strategy to validate the req

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthGuard } from '@nestjs/passport'
 
+import { AuthService} from '../services/auth.service';
+import { User } from 'src/modules/social-api/users/entities/user.entity';
 @Controller('auth')
 export class AuthController {
+    constructor(private authService: AuthService) { }
 
     @UseGuards(AuthGuard('local')) //Strategy local
     @Post('login')
+    @HttpCode(200)
     login(@Req() req: Request) {
+        //const user = req.user as User;
         return req.user;
     }
 }
--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -13,7 +13,7 @@ export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
     }
 
     async validate(email: string, password: string) {
-        const user = await this.authService.validateUser(email, password);
+        const user = await this.authService.validateUser(email, password); //
         if (!user) {
             throw new UnauthorizedException('not allow');
         }
