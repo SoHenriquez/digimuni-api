32674092f13b582d901f1e11a4e711b2e2c53e2e
SoHenriquez <estebansotohenriquez@gmail.com>
2022-12-12 17:04:24 -0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 
 import { SocialApiModule } from './modules/social-api/social-api.module';
 import { CommonModule } from './common/common.module';
-import { EnvConfiguration } from './config/env.config';
+
 import config from './config/config';
 
 import { AppController } from './app.controller';
@@ -11,12 +11,15 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { JoiValidationSchema } from './config/joi.validation';
 import { MongooseModule } from '@nestjs/mongoose';
+import { enviroments } from './enviroments';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      /* envFilePath: enviroments[process.env.NODE_ENV] || '.env', */
-      load: [EnvConfiguration, config],
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
       validationSchema: JoiValidationSchema,
+      
     }),
     MongooseModule.forRoot('mongodb://localhost:27017/digimuni-v2'),
     SocialApiModule,
